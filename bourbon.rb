7671cd669afddc69cab41deb5e701153bb0deadc
Nick Cappadona <nickypasta@gmail.com>
2015-09-11 00:00:32 -0400

    Don't copy the .git directory via 'wagon init'

--- bourbon.rb
+++ bourbon.rb
@@ -2,6 +2,16 @@ class Bourbon < Locomotive::Wagon::Generators::Site::Base
   def self.source_root
     File.join(File.dirname(__FILE__))
   end
+
+  # Don't copy .git directory when site template is used via 'wagon init'
+  def copy_sources
+    directory('.', self.destination, {
+      recursive:        true,
+      exclude_pattern:  /\.git\//,
+      name:             self.name,
+      version:          Locomotive::Wagon::VERSION
+    })
+  end
 end
 
 Locomotive::Wagon::Generators::Site.register(:bourbon, Bourbon, %{
